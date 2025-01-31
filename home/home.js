4df0a0831be3937244ab494f0199e3f8e51ff6e0
danielwanm <danielwang7341@gmail.com>
2025-01-31 19:58:15 +1100

    your commit message

--- home/home.js
+++ home/home.js
@@ -1,3 +1,10 @@
+if (!sessionStorage.getItem("key")) {
+    window.location.href = "index.html"
+}
+
+
+
+
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 const appSettings = {
