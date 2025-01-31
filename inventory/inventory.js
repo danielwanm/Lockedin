25ea390dd753d9fb65b5a85074bc6a3ef2a13cbe
danielwanm <danielwang7341@gmail.com>
2025-01-31 15:20:24 +1100

    your commit message

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -32,7 +32,7 @@ onValue(items, function(snapshot){
         backgrounds.append(newEl)
         newEl.addEventListener("click", function(){
             document.body.style.backgroundImage = `url('${itemsArray[i].url}')`; // Set as background
-        }
+        })
 
 
 
